e918ebfc3ddc695d6eeeab56b9be97ce59a2b569
tanersaydam <tanersaydam@gmail.com>
2023-03-22 18:37:01 +0300

    .

---
+++ WebApi/Controllers/ValuesController.cs
@@ -0,0 +1,21 @@
+﻿using Iyzipay.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OdemeSistemiService;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> Test()
+        {
+            RequestIyzico request = new();
+
+            Payment payment = OdemeService.IyzicoOdeme(request);
+            return Ok(payment);
+        }
+    }
+}
